eb8edaec35a6dcd633c7f90ab95f1901b88a22d5
Leandro <leandro.jacho2003@gmail.com>
2026-04-18 14:14:32 -0500

    segundo programa uso de void, usando descuento del 10%

--- practica 2 void/src/Producto.java
+++ practica 2 void/src/Producto.java
@@ -34,12 +34,26 @@ public class Producto {
 
     }
 
+    public double descuento(){
+        return total * 10/100;
+    }
+
+    public double descuentoaplicado(){
+        if (total > 1000){
+            return total - descuento();
+        }else{
+            System.out.println("No aplica descuento");
+            return 0;
+        }
+    }
+
     public void factura(){
         System.out.println("=======Factura=======");
         System.out.println("Nombre: "+nombreproducto);
         System.out.println("Precio: "+precio);
         System.out.println("Cantidad: "+cantidad);
         System.out.println("Total: "+total);
+        System.out.println("Total con descuento: "+descuentoaplicado());
         System.out.println("=======================");
     }
 }
